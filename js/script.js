36f7332d8f3db63c273b1acb583be075c0c786f9
ryyyzer <74414635+ryyyzer@users.noreply.github.com>
2023-12-29 19:15:30 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-  window.onload = function() {
-    var count = 3;
-    var textElement = document.getElementById('countdown');
-    var countdown = setInterval(function() {
-        textElement.textContent = count;
-        count--;
-        if (count < 0) {
-            clearInterval(countdown);
-            window.location.replace('https://cdn.vvvfast.fun');
-        }
-    }, 1000);
-  };
+window.onload = function() {
+  var count = 3;
+  var textElement = document.getElementById('countdown');
+  var countdown = setInterval(function() {
+      textElement.textContent = count;
+      count--;
+      if (count < 0) {
+          clearInterval(countdown);
+          window.location.replace('https://cdn.vvvfast.fun');
+      }
+  }, 1000);
+};
+
+document.addEventListener("click", function() {
+    window.location.replace = "https://cdn.vvvfast.fun";
+      });
