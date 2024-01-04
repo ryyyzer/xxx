fb03cef62edcb07cb0e2c22088a397a0528d9e0a
ryyyzer <74414635+ryyyzer@users.noreply.github.com>
2024-01-04 15:57:41 +0800

    更新 script.js

--- js/script.js
+++ js/script.js
@@ -20,3 +20,30 @@ window.onload = function() {
       }
   }, 1000);
 };
+
+function Countdown(targetDate, elementId) {
+  this.targetDate = targetDate;
+  this.elementId = elementId;
+}
+
+Countdown.prototype.getTotalMinutes = function() {
+  var now = new Date();
+  var diff = Math.floor((this.targetDate - now) / 1000);
+  var minutes = Math.floor(diff / 60);
+  return minutes > 0 ? minutes : 0;
+};
+
+Countdown.prototype.updateDisplay = function() {
+  var minutes = this.getTotalMinutes();
+
+  var countdownElement = document.getElementById(this.elementId);
+  countdownElement.innerHTML = "距离官网恢复" + minutes + "分钟";
+};
+
+Countdown.prototype.start = function() {
+  this.updateDisplay();
+  var self = this;
+  setInterval(function() {
+    self.updateDisplay();
+  }, 1000);
+};
